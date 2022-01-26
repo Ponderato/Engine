59ebe3e272675331f32858a5af665247a5825d63
Ponderato <68611385+Ponderato@users.noreply.github.com>
2022-01-27 00:20:05 +0100

    first triangle!

--- appGLFW/mainProg.cpp
+++ appGLFW/mainProg.cpp
@@ -4,19 +4,50 @@
 
 #include <iostream>
 #include <stdio.h>
-#include "oglContext.h"
+//#include "../engine/oglContext.h"
+#include <vector>
+
+std::vector<std::string> lines;
+const char** content;
+size_t size;
+
+//Vertex data
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+};
+
+float verticesRect[] = {
+	 0.5f,  0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	-0.5f, -0.5f, 0.0f,
+	-0.5f,  0.5f, 0.0f
+};
+
+GLuint indices[] = {
+	0, 1, 3,
+	1, 2, 3
+};
+
+GLuint VBO, VBOrect;
+GLuint VAO, VAOrect;
+GLuint shaderProgram;
+
+GLuint EBO;
 
 //Declaration of methods -> C programming stuff :D
 GLFWwindow* initContext();
+void initData();
+void initShaders();
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
-
-int main()
-{
-
+int main(){
 	GLFWwindow* window = initContext();
+	initData();
+	initShaders();
 
 	//render loop
 	while (!glfwWindowShouldClose(window)) {
@@ -26,6 +57,10 @@ int main()
 
 		//rendering commands
 		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAOrect);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		//check and call events and swap buffers
 		glfwSwapBuffers(window);
@@ -52,26 +87,127 @@ GLFWwindow* initContext() {
 	}
 	glfwMakeContextCurrent(window);
 
-	initGLEW();
+	//initGLEW();
+	//We initialize glew. GLEW sets the pointer functions for your platform.
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		printf("Error: %s\n", glewGetErrorString(err));
+		exit(-1);
+	}
 
 	//Viewport coordinates and callback function for resize.
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	glClearColor(0.6f, 1.0f, 0.0f, 0.3f);
+	glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
 
 	return window;
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
+void initData() {
+	glGenVertexArrays(1, &VAO);
+	glGenVertexArrays(1, &VAOrect);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &VBOrect);
+	glGenBuffers(1, &EBO);
+	
+	//Bind VAO
+	glBindVertexArray(VAO);
+	//Copy our vertices array in a vertex buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//Set the vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(VAOrect);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOrect);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesRect), verticesRect, GL_STATIC_DRAW);
+	//Copy our index array in an element buffer for OpenGL to use
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	//Unbind VAO
+	glBindVertexArray(0);
+}
+
+void readFile(const char* filename) {
+	FILE* f;
+	fopen_s(&f, filename, "rt");
+	if (!f) {
+		printf("ERROR: Can't open filename: %s\n", filename);
+	}
+	char line[256];
+	while (fgets(line, 256, f)) lines.push_back(line);
+	fclose(f);
+	size = (int)lines.size();
+	content = new const char* [size];
+	for (int i = 0; i < size; i++) {
+		content[i] = lines[i].c_str();
+	}
+}
+
+void checkCompilling(GLuint shader) {
+	GLint isCompiled = 0;
+	GLint length;
+	GLchar* log;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &isCompiled);
+	if (!isCompiled)
+	{
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+		log = new GLchar[length];
+		glGetShaderInfoLog(shader, length, &length, log);
+		printf("ERROR: Compile error: %s\n", log);
+		delete log;
+	}
+}
+
+GLuint createShader(const char* fileName, GLenum shaderType) {
+	GLuint shader = glCreateShader(shaderType);
+	readFile(fileName);
+
+	glShaderSource(shader, size, content, NULL);
+	glCompileShader(shader);
+
+	delete[] content;
+	lines.clear();
+
+	checkCompilling(shader);
+
+	return shader;
+}
+
+void initShaders() {
+	GLuint vs = createShader("vertexShader_1.vs", GL_VERTEX_SHADER);
+	GLuint fs = createShader("fragmentShader_1.fs", GL_FRAGMENT_SHADER);
+
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vs);
+	glAttachShader(shaderProgram, fs);
+
+	glLinkProgram(shaderProgram);
+	//can also check if linking a shader program failed.
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
 	glViewport(0, 0, width, height);
 }
 
 //Checks if esc key have been pressed and if so, "closes" the window
-void processInput(GLFWwindow* window)
-{
+void processInput(GLFWwindow* window){
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	}
 }
