43797a46b4cba978bf25f17c94a8b2149cf45bca
Ponderato <68611385+Ponderato@users.noreply.github.com>
2022-01-31 14:18:40 +0100

    3D cube

--- appGLFW/mainProg.cpp
+++ appGLFW/mainProg.cpp
@@ -1,7 +1,9 @@
 #include <GL/glew.h>
 #include <glfw3.h>
-//glfw is like glut, used for context creation, window stuff, ...
 #include "stb_image.h"
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
 
 #include <iostream>
 #include <stdio.h>
@@ -11,35 +13,66 @@ std::vector<std::string> lines;
 const char** content;
 size_t size;
 
-//Vertex data
+//Data
+//glm::mat4 model_M = glm::mat4(1.0f);
+glm::mat4 view_M = glm::mat4(1.0f);
+glm::mat4 proj_M = glm::mat4(1.0f);
+
 float vertices[] = {
-	 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
-	-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
-	 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
-float verticesRect[] = {
-	 0.5f,  0.5f, 0.0f,    1.0f,  0.0f, 0.0f,
-	 0.5f, -0.5f, 0.0f,    0.0f,  1.0f, 0.0f,
-	-0.5f, -0.5f, 0.0f,    0.0f,  0.0f, 1.0f,
-	-0.5f,  0.5f, 0.0f,    1.0f,  1.0f, 0.0f
-};
-
-GLuint indices[] = {
-	0, 1, 3,
-	1, 2, 3
-};
-
-GLuint VBO, VBOrect;
-GLuint VAO, VAOrect;
+GLuint VBO;
+GLuint VAO;
 GLuint shaderProgram;
 
-GLuint EBO;
+//GLuint EBO;
 
-int width;
-int height;
-int nrChannels;
-unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+GLuint texture1, texture2;
+
+float faceVisibility = 0.2;
+
+int width, height, nrChannels;
 
 //Declaration of methods -> C programming stuff :D
 GLFWwindow* initContext();
@@ -102,44 +135,89 @@ GLFWwindow* initContext() {
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	//TODO -> move initOpenGL stuff to a new method void initOGL();
+	glEnable(GL_DEPTH_TEST);
 	glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
 
 	return window;
 }
 
 void initData() {
-	//glGenVertexArrays(1, &VAO);
-	//glGenBuffers(1, &VBO);
-	//
-	////Bind VAO
-	//glBindVertexArray(VAO);
-	////Copy our vertices array in a vertex buffer for OpenGL to use
-	//glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	////Set the vertex attributes pointers for position
-	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
-	//glEnableVertexAttribArray(0);
-	////color
-	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
-	//glEnableVertexAttribArray(1);
 
 	//Rectangle
-	glGenVertexArrays(1, &VAOrect);
-	glGenBuffers(1, &VBOrect);
-	glGenBuffers(1, &EBO);
-	glBindVertexArray(VAOrect);
-	glBindBuffer(GL_ARRAY_BUFFER, VBOrect);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesRect), verticesRect, GL_STATIC_DRAW);
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	//glGenBuffers(1, &EBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	//Copy our index array in an element buffer for OpenGL to use
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
+	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	//glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 	//Unbind VAO
 	glBindVertexArray(0);
+
+	//Textures
+	stbi_set_flip_vertically_on_load(true);
+	//texture1
+	unsigned char* data1 = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+	
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	if (data1){
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data1);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	//Free the image memory -> Good practice
+	stbi_image_free(data1);
+
+	//texture2
+	unsigned char* data2 = stbi_load("face.png", &width, &height, &nrChannels, 0);
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	if (data2) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data2);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	stbi_image_free(data2);
+
+	//Bind textures to the corresponding texture unit
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	glActiveTexture(GL_TEXTURE0 + 1);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	//Matrices
+	//model_M = glm::rotate(model_M, glm::radians(-55.0f), glm::vec3(1.0, 0.0, 0.0));
+	view_M = glm::translate(view_M, glm::vec3(0.0, 0.0, -3.0));
+	proj_M = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 }
 
 void readFile(const char* filename) {
@@ -199,18 +277,35 @@ void initShaders() {
 
 	glLinkProgram(shaderProgram);
 	//can also check if linking a shader program failed.
+
+	//Uniforms -> AFTER glUseProgram(); !!!!!!!!!
+	//uniforms need the program to be linked to the context to 
+	glUseProgram(shaderProgram);
+	//the final number -> texture unit each uniform sampler correpsonds to
+	glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0);
+	glUniform1i(glGetUniformLocation(shaderProgram, "texture2"), 1);
+
+	//location, how many matrices we want to pass, transpose, data as glm::value_ptr
+	
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "viewM"), 1, GL_FALSE, glm::value_ptr(view_M));
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projM"), 1, GL_FALSE, glm::value_ptr(proj_M));
 }
 
 void render() {
-	glClear(GL_COLOR_BUFFER_BIT);
+	glm::mat4 model_M = glm::mat4(1.0f);
+	model_M = glm::rotate(model_M, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5, 1.0, 0.0));
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glUseProgram(shaderProgram);
 
-	//Uniforms here AFTER glUseProgram(); !!!!!!!!!!!!
+	//this goes here in order to the arrow keys to work 
+	glUniform1f(glGetUniformLocation(shaderProgram, "faceV"), faceVisibility);
 
-	glBindVertexArray(VAOrect);
-	//glDrawArrays(GL_TRIANGLES, 0, 3);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "modelM"), 1, GL_FALSE, glm::value_ptr(model_M));
 
+	glBindVertexArray(VAO);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
@@ -229,5 +324,13 @@ void processInput(GLFWwindow* window){
 
 	if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+		faceVisibility += 0.1;
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+		faceVisibility -= 0.1;
 	}
 }
--- build/appGLFW/vertexShader.vs
+++ build/appGLFW/vertexShader.vs
@@ -1,12 +1,19 @@
 #version 330 core
 
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
+//layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
 
-out vec3 color;
+//out vec3 color;
+out vec2 texCoord;
+
+uniform mat4 modelM;
+uniform mat4 viewM;
+uniform mat4 projM;
 
 void main()
 {
-	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-	color = aColor;
+	gl_Position =  projM * viewM * modelM * vec4(aPos, 1.0);
+	//color = aColor;
+	texCoord = aTexCoord;
 }
